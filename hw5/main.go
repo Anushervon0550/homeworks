4bef6807729b1cab93cc6ad8f14ab213359c631e
Anushervon0550 <odinaevanuservon21@gmail.com>
2024-07-22 14:00:06 +0500

    Add files via upload

---
+++ hw5/main.go
@@ -0,0 +1,199 @@
+package main
+
+// import "fmt"
+
+func main() {
+// 	for i := 0; i <= 10; i++ {
+// 		fmt.Println(i)
+// 	}
+// for i:=1; i<=5;i++{
+// 	fmt.Println(i*i)
+// }
+// for i:=1;i<=10;i++{
+// 	fmt.Println(i*3)
+// }
+// n1,n2:=0,1
+// for i:=0; i<=10; i++{
+// 	n3:=n1+n2
+// 	n1=n2
+// 	n2=n3
+// 	fmt.Println(n3)
+// }
+// a:=6
+// b:=544
+// for b != 0 {
+// 	a%= b
+// 	if a== 0 {
+// 		fmt.Println(b)
+// 	}
+// 	b %=a
+// 	fmt.Println(a)
+// }
+
+// for i:=1; i <= 100 ;i++{
+// 	if i%3 ==0 {
+// 		println("Fizz ")
+// 	}else if i%5==0{
+// 		println("Buzz ")
+// 	}else if i%3 == 0 && i%5 == 0{
+// 		println("FizzBuzz")
+// 	}
+// 	println(i)
+// }
+// var a int = 4
+// if a%2 == 0 && a%3 == 0{
+// 	println("число не является простым")
+// }else {
+// 	println("число является простым")
+// }
+// var a int = 88
+// for i:=1; i<=a; i++{
+// 	if a%i == 0{
+// 		print(i)
+// 	}
+// // }
+// var a int =56
+// var b float32
+// if a>0 {
+// 	b= float32(a%10)
+// 	a/=10
+// }
+// println(int(float32(a)+b))
+// var n int
+	// for {
+	// 	fmt.Scan(&n)
+	// 	if n < 0 {
+	// 		fmt.Println("введите положительное число ")
+	// 		continue
+	// 	} else {
+	// 		fmt.Println("вы ввели положительное число ")
+	// 		break
+	// 	}
+	// }
+// var n int
+	// a := 6
+	// fmt.Scan(&n)
+	// for i := 1; i <= n; i++ {
+	// 	a *= i
+	// 	if i > 1000 {
+	// 		break
+	// 	} else {
+	// 		fmt.Println(a)
+	// 		continue
+	// 	}
+	// }
+	// var a int = 54
+	// var b int
+	// for {
+	// 	fmt.Scan(&b)
+	// 	if a == b {
+	// 		fmt.Println("число введено верно ")
+	// 		break
+	// 	} else {
+	// 		fmt.Println("введите число заного ")
+	// 		continue
+	// 	}
+	// }
+// var n int
+	// fmt.Scan(&n)
+	// for i := 1; i <= n; i++ {
+	// 	a := strings.Repeat(" ", n-i)
+	// 	b := strings.Repeat("*", 2*i-1)
+	// 	fmt.Println(a + b)
+	// }
+		// var n int
+	// fmt.Scan(&n)
+	// for i := 1; i <= n; i++ {
+	// 	for j := 1; j < 10; j++ {
+	// 		fmt.Printf("%d * %d = %d \n", i, j, i*j)
+	// 	}
+	// 	fmt.Printf("__________________")
+	// }
+// var n int
+	// fmt.Scan((&n))
+	// for i := 1; i < n; i++ {
+	// 	var a int = 1
+	// 	for j := 0; j <= i; j++ {
+	// 		if j == 0 || j == i {
+	// 			fmt.Println(1, " ")
+	// 		} else {
+	// 			b := a + a
+	// 			fmt.Println(b, " ")
+	// 			a = b
+	// 		}
+	// 	}
+	// 	fmt.Println()
+	// }
+		// for {
+	// 	var n int
+	// 	fmt.Scan(&n)
+	// 	if n < 0 {
+	// 		break
+	// 	}
+	// 	f := 1
+	// 	for i := 1; i <= n; i++ {
+	// 		f *= i
+	// 	}
+	// 	fmt.Println("факториал числа ", n, " равно ", f)
+	// }
+
+	// for {
+	// 	var a, b int
+	// 	fmt.Scanln(&a, &b)
+	// 	fmt.Println("сумма этих чисел равна ", a+b)
+	// 	break
+	// }
+	
+	// for i := 1; i <= 100; i++ {
+	// 	if i%int(math.Sqrt(float64(i))) != 0 {
+	// 		fmt.Println(i)
+	// 	}
+	// }
+		// var n int
+	// if n <= 1 {
+	// 	fmt.Println(false)
+	// }
+	// for i := 1; i <= 50; i++ {
+	// 	a := true
+	// 	for j := 2; j*j <= i; j++ {
+	// 		if i%j == 0 {
+	// 			a = false
+	// 			break
+	// 		}
+	// 	}
+	// 	if a {
+	// 		fmt.Println(i)
+	// 	}
+	// }
+	// var a int = 2
+	// var b int = 3
+	// for i := 1; i <= 30; i++ {
+	// 	if i%a == 0 || i%b == 0 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+// for i := 1; i <= 50; i++ {
+	// 	if math.Abs(float64(i))*float64(i) <= 50 {
+	// 		fmt.Println(i)
+	// 	} else {
+	// 		break
+	// 	}
+	// }
+	// a := 
+	// for i := 1; i <= 100; i++ {
+	// 	if a+i > 200 {
+	// 		break
+	// 	}
+	// 	fmt.Println(i)
+	// 	a += i
+	// }
+	
+	// 	for {
+	// 		var a int
+	// 		fmt.Scan(&a)
+	// 		if a%7 == 0 {
+	// 			break
+	// 		}
+	// 	}
+}
